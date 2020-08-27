bee6227a1b57930e8ae8cc06adeb633997a76d5d
MeowNya <Evina666@gmail.com>
2020-08-27 15:23:11 +0500

    Server is multithreading now

--- Server.rb
+++ Server.rb
@@ -10,25 +10,33 @@ port = 2000
 
 server = TCPServer.open(host, port)    # Socket to listen on port 2000
 puts "Server is starting http://#{host}:#{port}"
-loop {
+loop do
+  # TODO: ловить исключения
+
   # begin
   #   raise MyCustomException.new "Message, message, message", "Yup"
   # rescue MyCustomException => e
   #   puts e.message # Message, message, message*
   #                  puts e.exception_type # Yup*
   # end
-  client = server.accept # Wait for a client to connect
-  _, remote_port, _, remote_ip = client.peeraddr
+  Thread.start(server.accept) do |client|
+    _, remote_port, _, remote_ip = client.peeraddr
 
-  puts "Client successfully connected #{remote_ip}:#{remote_port}"
-  msg = recv_all(client)
-  command, args = parse_command(msg)
-  puts "Command: '#{command}', args: '#{args}'"
+    puts "Client successfully connected #{remote_ip}:#{remote_port}"
+    msg = recv_all(client)
+    command, args = parse_command(msg)
+    puts "Command: '#{command}', args: '#{args}'"
 
-  rs = process_command(command, args)
-  send_all(client, rs)
+    # if command == 'color' then
+    #   sleep 5
+    # end
 
-  client.close
-  puts 'Client is disconnected'
-  puts "\n"
-}
+    rs = process_command(command, args)
+    puts "Response: #{rs}"
+    send_all(client, rs)
+
+    client.close
+    puts 'Client is disconnected'
+    puts "\n"
+  end
+end
